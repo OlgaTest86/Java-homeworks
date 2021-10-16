d5949247007b00187690af2b6713f6d50ced42d8
Dudkina Olga <dudkinaolia@yandex.ru>
2021-10-16 23:00:05 +0300

    Money transfer

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int totalSum = balance + transferAmount;
+        System.out.println(totalSum);
+    }
 }
